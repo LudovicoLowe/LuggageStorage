70770dfd68627616b3724a6ec5b5991f29717188
paolorollo <rollo.1713885@studenti.uniroma1.it>
2018-06-05 11:40:02 +0200

    Added logo, fixed navbar, added delete options

--- project/app/controllers/places_controller.rb
+++ project/app/controllers/places_controller.rb
@@ -1,6 +1,6 @@
 class PlacesController < ApplicationController
   before_action :set_place, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:show, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:edit, :update, :destroy]
   before_action :correct_user, only: [:edit, :update, :destroy]
 
   # GET /places
